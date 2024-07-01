b574d564ca6fc15d6019d648ed9735fbb47d2c42
Victor Torres <clerossephirot@gmail.com>
2024-07-01 09:17:22 -0300

    classe possition criada

--- src/main/java/org/vtorres/boardgame/Position.java
+++ src/main/java/org/vtorres/boardgame/Position.java
@@ -29,4 +29,9 @@ public class Position {
     public void setColumn(int column) {
         this.column = column;
     }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
 }
