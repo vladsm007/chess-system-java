ed936848f33a25314f46bf76faa451c72e41924b
Victor Torres <clerossephirot@gmail.com>
2024-07-02 08:50:35 -0300

    board criado

--- src/main/java/org/vtorres/Main.java
+++ src/main/java/org/vtorres/Main.java
@@ -1,11 +1,13 @@
 package org.vtorres;
 
+import org.vtorres.boardgame.Board;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-
+        Board board = new Board(8, 8);
 
     }
 }
